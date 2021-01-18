9cfdc09738dbe607ea7a98f3f0ef99a33bde9efc
amahalik17 <amahalik@student.cccd.edu>
2021-01-18 10:52:53 -0800

    added some scaffolding for our project, as well as the sql code for our db tables

---
+++ project2_sql_db.sql
@@ -0,0 +1,65 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/Ue4cxH
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Physical
+
+CREATE TABLE "data_table" (
+    "id" serial   NOT NULL,
+    "valence" FLOAT   NOT NULL,
+    "year" INT   NOT NULL,
+    "acousticness" FLOAT   NOT NULL,
+    "artists" VARCHAR(100)   NOT NULL,
+    "danceability" FLOAT   NOT NULL,
+    "duration_ms" INT   NOT NULL,
+    "energy" FLOAT   NOT NULL,
+    "explicit" INT   NOT NULL,
+    "instrumentalness" VARCHAR(30)   NOT NULL,
+    "key" INT   NOT NULL,
+    "liveness" FLOAT   NOT NULL,
+    "loudness" FLOAT   NOT NULL,
+    "mode" INT   NOT NULL,
+    "name" VARCHAR(100)   NOT NULL,
+    "popularity" INT   NOT NULL,
+    "release_date" VARCHAR(20)   NOT NULL,
+    "speechiness" FLOAT   NOT NULL,
+    "tempo" FLOAT   NOT NULL,
+    CONSTRAINT "pk_data_table" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "year_table" (
+    "mode" INT   NOT NULL,
+    "year" INT   NOT NULL,
+    "acousticness" FLOAT   NOT NULL,
+    "danceability" FLOAT   NOT NULL,
+    "duration_ms" FLOAT   NOT NULL,
+    "energy" FLOAT   NOT NULL,
+    "instrumentalness" FLOAT   NOT NULL,
+    "liveness" FLOAT   NOT NULL,
+    "loudness" FLOAT   NOT NULL,
+    "speechiness" FLOAT   NOT NULL,
+    "tempo" FLOAT   NOT NULL,
+    "valence" FLOAT   NOT NULL,
+    "popularity" FLOAT   NOT NULL,
+    "key" INT   NOT NULL
+);
+
+CREATE TABLE "genre_table" (
+    "mode" INT   NOT NULL,
+    "genres" VARCHAR(100)   NOT NULL,
+    "acousticness" FLOAT   NOT NULL,
+    "danceability" FLOAT   NOT NULL,
+    "duration_ms" FLOAT   NOT NULL,
+    "energy" FLOAT   NOT NULL,
+    "instrumentalness" FLOAT   NOT NULL,
+    "liveness" FLOAT   NOT NULL,
+    "loudness" FLOAT   NOT NULL,
+    "speechiness" FLOAT   NOT NULL,
+    "tempo" FLOAT   NOT NULL,
+    "valence" FLOAT   NOT NULL,
+    "popularity" FLOAT   NOT NULL,
+    "key" INT   NOT NULL
+);
+
